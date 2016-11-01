43377b7d8dfd24c17b3e3f0a132beb59ba492012
Rob Kovtiuk <robsonheater@gmail.com>
2016-11-01 23:01:09 +0200

    Fix merge old versions to one

---
+++ app/src/main/java/model/Trim.java
@@ -0,0 +1,14 @@
+package model;
+
+
+import org.apache.pig.PrimitiveEvalFunc;
+
+//cc Trim An EvalFunc UDF to trim leading and trailing whitespace from chararray values
+//vv Trim
+public class Trim extends PrimitiveEvalFunc<String, String> {
+    @Override
+    public String exec(String input) {
+        return input.trim();
+    }
+}
+// ^^ Trim
